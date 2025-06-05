e0847fc35bbc4180dc56c6286f7a7b45f4ca053b
sivasankar55 <sivasankar8875@gmail.com>
2025-06-05 12:57:11 +0530

    models and signup function

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,63 @@
+ import User from "../models/User.js";
+ import jwt from "jsonwebtoken";
+
+export async function signup  (req, res)  {
+    const {email, password, fullName} = req.body;
+
+    try {
+        if(!email || !password || !fullName){
+            return res.status(400).json({message:"All fields are required"});
+        }
+        
+        if(password.length <6) {
+            return res.status(400).json({message:"Password must be ate least 6 characters"});
+        }
+
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+if (!emailRegex.test(email)) {
+  return res.status(400).json({ message: "Invalid email format" });
+}
+
+    const existingUser = await User.findOne({email});
+    if(existingUser){
+        return res.status(400).json({message:"User already exists, please use a different one"});
+    }
+
+    const idx = Math.floor(Math.random()*100) + 1; //generate a num bw 1 and 100
+
+    const randomAvatar = `https://avatar.iran.liara.run/public/${idx}.png`
+
+    const newUser = await User.create({
+        email,
+        fullName,
+        password,
+        profilePic: randomAvatar,
+    })
+
+    const token = jwt.sign({userId:newUser._id},process.env.JWT_SECRET_KEY, {
+        expiresIn: "7d",
+    })
+
+    res.cookie("jwt",token,{
+        maxAge: 7 * 24 * 60 * 60 * 1000,
+        httpOnly: true,
+        sameSite :"strict",
+        secure: process.env.NODE_ENV === "production",
+    })
+
+    res.status(201).json({success:true, user:newUser})
+
+    } catch (error) {
+      console.log("Error in signup controller", error);
+      res.status(500).json({message:"Internal server error"});  
+    }
+};
+
+export async function login  (req, res)  {
+    res.send ('login');
+};
+
+export async function logout  (req, res)  {
+    res.send('logout');
+};
