53d9f7cf4cf76e9825e9f642a7f121db3a6fdba9
sivasankar55 <sivasankar8875@gmail.com>
2025-06-13 21:12:54 +0530

    added cors

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
-
+import cors from "cors";
 import authRoutes from "./routes/auth.route.js";
 import userRoutes from "./routes/user.route.js";
 import chatRoutes from "./routes/chat.route.js";
@@ -11,6 +11,11 @@ import { connectDB } from "./lib/db.js";
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT ;
+
+app.use(cors({
+    origin:"https://localhost:5173",
+    credentials: true // allow the frontend to send cookies
+}));
 
 app.use(express.json());
 app.use(cookieParser());
