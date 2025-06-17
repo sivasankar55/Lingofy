8ebc27ae91aacc524928aa2fe9811aec4d20344d
sivasankar55 <sivasankar8875@gmail.com>
2025-06-17 12:31:39 +0530

    onboarding Ui and functionality also hook

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import {Navigate, Route, Routes} from "react-router";
+import { Navigate, Route, Routes } from "react-router";
 
 import HomePage from "./pages/HomePage.jsx";
 import SignUpPage from "./pages/SignUpPage.jsx";
@@ -6,38 +6,76 @@ import LoginPage from "./pages/LoginPage.jsx";
 import NotificationsPage from "./pages/NotificationsPage.jsx";
 import CallPage from "./pages/CallPage.jsx";
 import OnboardingPage from "./pages/OnboardingPage.jsx";
-import  {Toaster} from "react-hot-toast";
+import { Toaster } from "react-hot-toast";
 import PageLoader from "./components/PageLoader.jsx";
 import useAuthUser from "./hooks/useAuthUser.js";
- 
-const App = () => {
- 
-const {isLoading,authUser} = useAuthUser();
-// authuser is an object to boolean
-const isAuthenticated = Boolean(authUser);
-const isOnboarded = authUser?.isOnboarding;
 
-  if (isLoading) return <PageLoader/>
+const App = () => {
+  const { isLoading, authUser } = useAuthUser();
+  // authuser is an object to boolean
+  const isAuthenticated = Boolean(authUser);
+  const isOnboarded = authUser?.isOnboarded;
+
+  if (isLoading) return <PageLoader />;
 
   return (
-   <div className="h-screen" data-theme="night">
-    <Routes>
-      <Route path="/" element={isAuthenticated && isOnboarded ? ( <HomePage/>) : (<Navigate to={!isAuthenticated ? "/login" : "/onboarding"}/>)}/>
+    <div className="h-screen" data-theme="night">
+      <Routes>
+        <Route
+          path="/"
+          element={
+            isAuthenticated && isOnboarded ? (
+              <HomePage />
+            ) : (
+              <Navigate to={!isAuthenticated ? "/login" : "/onboarding"} />
+            )
+          }
+        />
 
-      <Route path= "/signup" element = {!isAuthenticated ? <SignUpPage/> : <Navigate to="/"/>}/>
+        <Route
+          path="/signup"
+          element={!isAuthenticated ? <SignUpPage /> : <Navigate to="/" />}
+        />
 
-      <Route path= "/login" element = {!isAuthenticated ? <LoginPage/> : <Navigate to="/" />}/>
+        <Route
+          path="/login"
+          element={!isAuthenticated ? <LoginPage /> : <Navigate to="/" />}
+        />
 
-      <Route path= "/notifications" element = {isAuthenticated ? <NotificationsPage/> : <Navigate to="/login" />}/>
+        <Route
+          path="/notifications"
+          element={
+            isAuthenticated ? <NotificationsPage /> : <Navigate to="/login" />
+          }
+        />
 
-      <Route path= "/call" element ={isAuthenticated ? <CallPage/> : <Navigate to="/login" />}/>
-      
-      <Route path="/call" element={isAuthenticated ? <CallPage/>: <Navigate to="/login" />} />
+        <Route
+          path="/call"
+          element={isAuthenticated ? <CallPage /> : <Navigate to="/login" />}
+        />
 
-      <Route path= "/onboarding" element ={ isAuthenticated ?  <OnboardingPage/> : <Navigate to="/login" /> } />
-    </Routes>
-    <Toaster/>
-   </div>
+        <Route
+          path="/call"
+          element={isAuthenticated ? <CallPage /> : <Navigate to="/login" />}
+        />
+
+        <Route
+          path="/onboarding"
+          element={
+            isAuthenticated ? (
+              !isOnboarded ? (
+                <OnboardingPage />
+              ) : (
+                <Navigate to="/" />
+              )
+            ) : (
+              <Navigate to="/login" />
+            )
+          }
+        />
+      </Routes>
+      <Toaster />
+    </div>
   );
 };
 
--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -8,4 +8,9 @@ export const signup = async (signupData) => {
 export const getAuthUser = async () => {
     const res = await axiosInstance.get("/auth/me");
     return res.data;
-  } 
+  }; 
+
+  export const completeOnboarding = async (userData) => {
+    const response = await axiosInstance.post("/auth/onboarding",userData);
+    return response.data;
+  }
