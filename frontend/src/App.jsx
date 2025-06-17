fbc371efec0cd3cd8b68624d14d03c2b2aaa9a55
sivasankar55 <sivasankar8875@gmail.com>
2025-06-17 22:00:17 +0530

    onboarding UI and functionality with custome hook

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -34,12 +34,12 @@ const App = () => {
 
         <Route
           path="/signup"
-          element={!isAuthenticated ? <SignUpPage /> : <Navigate to="/" />}
+          element={!isAuthenticated ? <SignUpPage /> : <Navigate to={isOnboarded ? "/" : "/onboarding"} />}
         />
 
         <Route
           path="/login"
-          element={!isAuthenticated ? <LoginPage /> : <Navigate to="/" />}
+          element={!isAuthenticated ? <LoginPage /> : <Navigate to={isOnboarded ? "/" : "/onboarding"} />}
         />
 
         <Route
